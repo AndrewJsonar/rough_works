4f6e1f86b0aa031001944e3cdc0fccbefc87b118
CuriousRedBeard <36869702+CuriousRedBeard@users.noreply.github.com>
2020-03-20 16:29:04 -0700

    made main.sh added python script call

--- g2_feed_updater/main.sh
+++ g2_feed_updater/main.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 # Andrew Ebl Mar 20 2020
 
-NUMBER_OF_ARGS=2
-CONNECTION_STRING=$1
-DATABASE=$2
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+number_of_args=2
+connection_string=$1
+database=$2
 
 help_function() {
-  echo "Argument 1 is connection string"
-  echo "Argument 2 is database to use"
+  echo "Argument 1 is the connection string, example: 'mongodb://user:password@localhost:27117/admin'"
+  echo "Argument 2 is the database to use"
 }
 
-if [ "$#" -ne "$NUMBER_OF_ARGS" ]; then
-  echo "needs $NUMBER_OF_ARGS arguemnts"
+if [ "$#" -ne "$number_of_args" ]; then
+  echo "needs $number_of_args arguemnts"
   help_function
+  exit
 fi
 
+echo $(date -u +%FT%T.%3NZ) - Starting reformater:
+python parent_path/reformater.py "$1" "$2"
+
+echo $(date -u +%FT%T.%3NZ) - Done!
